c19c203c575553d65536fb1ad40c11ef36ccff23
Ayyappan-1 <m.ayyappan1310@gmail.com>
2025-12-11 12:31:20 +0530

    Updated : Improved Loop In Insertion At Beginning

--- Arrays/Insert_At_Beginning.py
+++ Arrays/Insert_At_Beginning.py
@@ -1,1 +1,14 @@
-print("yes")
+# My first program and it is for dynamic array
+def insert(b):
+    n=len(a)
+    a.append(None)
+    for i in range(n-1,-1,-1):
+       a[i+1]=a[i]
+    a[0]=b
+     
+a=[1,2,3]
+b=int(input("Enter the number to be inserted"))
+insert(b)
+print(a)
+    
+    
