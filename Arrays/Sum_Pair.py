039a9a98362feb98161f1dd7bfd4204a304deed3
Ayyappan-1 <m.ayyappan1310@gmail.com>
2025-12-21 21:49:58 +0530

    Updated : Sum Pair By Using Hash-Set

--- Arrays/Sum_Pair.py
+++ Arrays/Sum_Pair.py
@@ -1,11 +1,28 @@
+# Element by element search method which has time complexity of O(n^2)
 def sum_pair(tar):
+    pairs=[]
     for i in range(len(a)):
         for j in range(i+1,len(a)):
            if (a[i]+a[j]==tar):
               pairs.append([a[i],a[j]])
+    return pairs
                 
-pairs=[]
+# Using Single loop with effective search (Hash-set) which as time complexity of O(n)
+def optimized(tar):
+    visited=set()
+    pair=[]
+    for i in a:
+        seen=tar-i
+        if(seen in visited):
+            pair.append([seen,i]) 
+        visited.add(i)         
+    return pair
+     
+
 a=[1,2,3,4,5]
+p1,p2=[],[]
 t=int(input("Enter the target element :"))
-sum_pair(t)
-print (pairs)
+p1=sum_pair(t)
+p2=optimized(t)
+print ("Brute Force :",p1)
+print("Optimized :",p2)
