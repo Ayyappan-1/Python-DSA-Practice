dacdd1b8520cacf1b8c5b12634ed9546a9e12c19
Ayyappan-1 <m.ayyappan1310@gmail.com>
2025-12-26 20:40:51 +0530

    Optimized : Left Rotation By Reversal Algorithm

--- Arrays/Left_Rotation.py
+++ Arrays/Left_Rotation.py
@@ -1,12 +1,28 @@
-# Brute force method for left rotation
-def left_rotation(a,d):
-    l=len(a)-1
+# Brute force method for left rotation which has the time complexity of O(n^2) and space Complexity O(1)
+def left_rotation(b,d):
+    l=len(b)-1
     for i in range(0,d):
-        a.append(None) 
-        a[l+1]=a[0]
-        a.pop(0)
-    print(a)
+        b.append(None) 
+        b[l+1]=b[0]
+        b.pop(0)
+    return b
+
+# Optimized method by using reversal algorithm which has the time complexity of O(n) and space complexity of O(1)
+def optimized(l,r):  
+    while(l<r):
+        a[l],a[r]=a[r],a[l]
+        l+=1
+        r-=1  
 
 a=[1,2,3,4,5,6,7,8,9]
-d=int(input("Enter Number of Rotation Needed :"))
-left_rotation(a,d)
+b=[]
+d=int(input("Enter Number of Rotation Needed :")) 
+l=len(a)-1
+d=d%(l+1)
+
+b=left_rotation(a.copy(),d)
+optimized(0,d-1)
+optimized(d,l)
+optimized(0,l)
+print("Brute Force Method : ",b)
+print("Optimized Method : ",a)
