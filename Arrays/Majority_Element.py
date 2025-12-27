654df2c68cef259ffcacc52bb7f037cc093e7e42
Ayyappan-1 <m.ayyappan1310@gmail.com>
2025-12-27 21:15:40 +0530

    Optimized : Majority Element using Boyer-Moore voting Algorithm

--- Arrays/Majority_Element.py
+++ Arrays/Majority_Element.py
@@ -1,4 +1,5 @@
-def majority_element():
+# Brute Force which has the time compelxity of O(n^2) and Space complexity of O(1)
+def majority_element(a):
     for i in range(len(a)):
         c=0
         for j in range(len(a)):
@@ -10,5 +11,29 @@ def majority_element():
         
     print("There is no majority element......")
 
+# Optimized method (Boyer-Moore Voting Algorithm) which has the Time complexity of O(n) and Space complexity of O(1)
+def optimized(a):
+    c=0
+    candidate=None
+    for i in a:
+        if(c==0):
+            candidate=i
+            c+=1
+        elif(candidate==i):
+            c+=1
+        else:
+            c-=1
+    c=0
+    for i in a:
+        if(i==candidate):
+            c+=1
+    return candidate, c
+
 a=[1,2,3,4,5,1,1,1,1]
-majority_element()
+majority_element(a)
+
+candidate,c=optimized(a)
+if(c>len(a)//2):
+      print(f"The majority Element is {candidate}")
+else:
+    print("There is no majority element found.....")
