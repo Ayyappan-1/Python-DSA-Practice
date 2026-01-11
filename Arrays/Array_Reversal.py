d580c101839542fe7f8510ff555a158f1ddb7e62
Ayyappan-1 <m.ayyappan1310@gmail.com>
2026-01-11 22:29:24 +0530

    Updated : Time & Space Complexity Is Added With Proper Structuring Of Code.

--- Arrays/Array_Reversal.py
+++ Arrays/Array_Reversal.py
@@ -1,10 +1,23 @@
-a=[1,2,3,4,5,6,7,8]
-l=len(a)
-le=l-1
-for i in range(0,l//2):
-    temp=a[i]
-    a[i]=a[le]
-    a[le]=temp
-    le-=1
-    print(a,i)
-     
+# Brute Force which has the time complexity of O(n) and space complexity of O(n)
+def array_reversal(a):
+    b=[]
+    for i in range(len(a)-1,-1,-1):
+        b.append(a[i])
+    return b
+
+# Optimized method by using reversal algorithm which has the time complexity of O(n) and Space complexity of O(1)
+def optimized(a):
+    l=len(a)
+    last_element=l-1
+    for i in range(0,l//2):
+        temp=a[i]
+        a[i]=a[last_element]
+        a[last_element]=temp
+        last_element-=1 
+    return a
+
+a,b=[1,2,3,4,5,6,7,8],[]
+b=array_reversal(a)
+print("Brute Force : ",b)
+a=optimized(a)
+print("Optimized Method :",a)
