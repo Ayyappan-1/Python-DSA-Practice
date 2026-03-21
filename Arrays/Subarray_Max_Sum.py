8cbf6aef2655150e0163231b06503049b51dd088
Ayyappan-1 <m.ayyappan1310@gmail.com>
2026-03-21 21:24:51 +0530

    Updated : Minor Code Fixes Applied.

--- Arrays/Subarray_Max_Sum.py
+++ Arrays/Subarray_Max_Sum.py
@@ -9,12 +9,12 @@ def subarray(a):
                b.append(s) 
     return max(b)
 
-# Optimized method (Kadane's Alogrithm)
+# Optimized method (Kadane's Alogrithm) which has the time complexity of O(n) and space complexity of O(1)
 def optimized(a):
-     max_sum,current_sum=a[0],0
+     max_sum,current_sum=float('-inf'),0
      for i in a:
           current_sum+=i
-          if(current_sum<a[i]):
+          if(current_sum<0):
                current_sum=0
           if(current_sum>max_sum):
                max_sum=current_sum
